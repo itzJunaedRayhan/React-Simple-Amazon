2a70f4585b9709d13aac4c75dc3fc492a811c02f
Junaed Rayhan <junaedrayhan@gmail.com>
2021-02-27 00:27:38 +0600

    font awesome added

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../../fakeData';
+import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
@@ -7,11 +8,9 @@ const Shop = () => {
     return (
         <div className="shop-container">
             <div className="product-container">
-                <ul>
                     {
-                        products.map(product => <li>{product.name}</li>)
+                        products.map(pd => <Product product = {pd}></Product>)
                     }
-                </ul>
             </div>
             <div className="cart-container">
                 <h3>This is cart</h3>
